fb1a05c34de9e74087191de721790adf22c696de
ebrahim <ebrahimali.cse.ru@gmail.com>
2022-03-18 20:01:51 +0600

    setup basic folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./sass/main.scss";
-import Home from "./components/pages/Home/Home"
+
+import Home from "./pages/Home/Home"
+import Login from './pages/admin/Login';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path='/admin' element={<Login />} />
           <Route exact path="/" element={<Home/>}></Route>
           {/* <Route exact path="/register" element={<Register />}></Route>
           <Route exact path="/dashboard" element={<Dashboard/>}></Route> */}
---
+++ client/src/pages/admin/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = ()  =>{
+  return (
+    <div>
+        <p style={{fontFamily: "Poppins"}}>Login</p>
+    </div>
+  )
+}
+
+export default Login
