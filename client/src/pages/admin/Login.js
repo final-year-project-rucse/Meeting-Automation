d3aa1c9d16b12db68f20634d57426d397c38e91e
ebrahim <ebrahimali.cse.ru@gmail.com>
2022-03-18 21:23:22 +0600

    IP design login page

--- client/src/pages/admin/Login.js
+++ client/src/pages/admin/Login.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Button from '../../components/button/Button'
+import Input from '../../components/input/Input'
+import { Link } from "react-router-dom"
 
 const Login = ()  =>{
   return (
-    <div>
-        <p style={{fontFamily: "Poppins"}}>Login</p>
+    <div className='login_container'>
+        <p className='login_title'>Meeting Automation</p>
+        <Input type="text" placeholder="E-Mail" name="email" />
+        <Input type="password" placeholder="Password" name="password" />
+        <Link to="forgotpassword" >Forgot Password</Link>
+        <div className='login_btn_container'>
+        <Button title="log in" />
+        
+        </div>
     </div>
+    
   )
 }
 
