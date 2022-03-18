63e0b8facb7a6f68218e7e1fed65d23fc946f158
Bepul-Hossain <bepul.cse.ru@gmail.com>
2022-03-18 18:06:58 +0600

    client added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./sass/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">
+          Left
+        </button>
+        <button type="button" class="btn btn-primary">
+          Middle
+        </button>
+        <button type="button" class="btn btn-primary">
+          Right
+        </button>
+      </div>
+      <div className="check">
+            jfhgfhfhgf
+      </div>
     </div>
   );
 }
