33e356a3e2e59ce899b1b1e98c2d8d0ea6e131f0
Bepul-Hossain <bepul.cse.ru@gmail.com>
2022-03-19 13:55:34 +0600

    Committee page added

--- client/src/pages/committee/Committee.js
+++ client/src/pages/committee/Committee.js
@@ -1,18 +1,58 @@
-function Committee() {
+const Committee = () => {
   return (
-    <div>
-      <ul class="nav">
-        <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page">
-            Committees
+    <div class="container">
+      <header className="topbar">
+        <div className="logo">
+          <a href="#" className="mylogo text">
+            Logo
           </a>
-        </li>
-        <li>
-          <a class="nav-item" aria-current="page" href="#">@add committee+</a>
-        </li>
-      </ul>
+        </div>
+        <nav className="navigation">
+          <ul>
+            <li>
+              <a href="#" className="text">Committee</a>
+            </li>
+            <li>
+              <a href="#" className="text">Create new committee</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="president">
+          <ul>
+            <li>
+              <a href="" className="text">President profile</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default Committee;
