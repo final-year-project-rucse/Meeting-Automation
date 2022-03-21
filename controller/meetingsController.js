d2434f4dfd7dae6f6268ff37cca0a446bff95dac
rakib-rs <rakib.rscse@gmail.com>
2022-03-21 18:55:50 +0600

    getMeeting by Id

--- controller/meetingsController.js
+++ controller/meetingsController.js
@@ -15,7 +15,8 @@ exports.getMeetings = (req, res) => {
   const committeeName = req.params.id;
   const meeting = committeeName + "meetings";
   const Meetings = mongoose.model(meeting, meetingSchema);
-  const query = Meetings.find({}).select('title -_id');
+  //const query = Meetings.find({}).select('title -_id');
+  const query = Meetings.find({}).select('title');
   query.exec((err, datas) => {
     if(err){
       return res.json(err);
@@ -33,7 +34,8 @@ exports.addMeeting = async (req, res) => {
   const meeting = committeeName + "meetings";
 
   const Meetings = mongoose.model(meeting, meetingSchema);
-  const { title, location, date, time, attendess, agendas } = req.body;
+  const { title, location, date, time, attendess, agendas,resolutions } = req.body;
+ // console.log(resolutions);
 
   newMeetingSchema = new Meetings({
     title: title,
@@ -42,6 +44,7 @@ exports.addMeeting = async (req, res) => {
     time: time,
     attendess: attendess,
     agendas: agendas,
+    resolutions: resolutions,
   });
   try {
     await newMeetingSchema.save().then((minutes) =>  res.json(minutes));
@@ -49,3 +52,32 @@ exports.addMeeting = async (req, res) => {
     res.json(err);
   }
 };
+
+exports.getMeetingById = async(req, res) => {
+  const objId = req.params.obj;
+  const committeeName = req.params.id;
+  const meeting = committeeName + "meetings";
+  const Meetings = mongoose.model(meeting, meetingSchema);
+  const query = Meetings.find({_id: objId});
+  query.exec((err,data) => {
+    if(err) {return res.status(404).json(err);}
+    return res.json({data:data});
+  })
+}
+
+exports.addResolution = async(req, res) => {
+  const committeeName = req.params.id;
+  const meeting = committeeName + "meetings";
+
+  const Meetings = mongoose.model(meeting, meetingSchema);
+  const {resolutions} = req.body;
+  const data ={
+    title:"rakib"
+  }
+//   console.log(resolutions);
+//   Meetings.resolutions.push(resolutions);
+//  await Meetings.save().then((data) => {return res => res.json({data: data});});
+
+  return res.json({data: resolutions});
+
+}
--- router/meeting.js
+++ router/meeting.js
@@ -7,5 +7,7 @@ const meetingsController = require("../controller/meetingsController");
 router.get("/:id/test", meetingsController.test);
 router.get("/:id/meetings", meetingsController.getMeetings);
 router.post("/:id/addMeeting", meetingsController.addMeeting);
+router.post("/:id/addResolution", meetingsController.addResolution);
+router.get("/:id/meetings/:obj",meetingsController.getMeetingById);
 
 module.exports = router;
