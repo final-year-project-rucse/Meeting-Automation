a3cde253e2328db00f494631ca7861e772d99d29
rakib-rs <rakib.rscse@gmail.com>
2022-03-23 16:39:32 +0600

    renamed

--- configure/example.js
+++ configure/example.js
@@ -1,4 +1,4 @@
-/// first rename example.js file to emailConfig;
+/// first rename example.js file to emailConfig.js;
 module.exports ={
     emailFrom:'your gmail',
     password:'your gmail password'
