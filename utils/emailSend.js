93ba26f3635bc0000eea79b582a26bb0df09ad3b
rakib-rs <rakib.rscse@gmail.com>
2022-03-23 15:56:58 +0600

    email send success

---
+++ utils/emailSend.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+const { emailFrom, password } = require("../configure/emailConfig");
+console.log(emailFrom);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: emailFrom,
+    pass: password,
+  },
+});
+
+module.exports = function Mail(data) {
+  const mailOptions = {
+    from: emailFrom,
+    to: data.to,
+    subject: data.subject,
+    html: data.html,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return error;
+    } else {
+      console.log("Email sent: " + info.response);
+      return info.response;
+    }
+  });
+};
+// const mailOptions2 = {
+//   from: 'meetingautomation.ru@gmail.com',
+//   to: 'bepul.cse.ru@gmail.com',
+//   subject: 'Invoices due',
+//   text: 'hiii'
+// };
+
+// transporter.sendMail(mailOptions, function (error, info) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Email sent: " + info.response);
+//   }
+// });
